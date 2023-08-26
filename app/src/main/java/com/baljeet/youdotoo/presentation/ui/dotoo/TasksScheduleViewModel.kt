2798b93349446f39558bcbb1964568a6fade8ebc
Baljeet singh <Baljeet808singh@gmail.com>
2023-08-26 12:00:40 -0300

    roles fixed for task schedule view model

--- app/src/main/java/com/baljeet/youdotoo/presentation/ui/dotoo/TasksScheduleViewModel.kt
+++ app/src/main/java/com/baljeet/youdotoo/presentation/ui/dotoo/TasksScheduleViewModel.kt
@@ -1,7 +1,12 @@
 package com.baljeet.youdotoo.presentation.ui.dotoo
 
 import androidx.lifecycle.ViewModel
-import com.baljeet.youdotoo.common.SharedPref
+import com.baljeet.youdotoo.common.Roles
+import com.baljeet.youdotoo.common.getRole
+import com.baljeet.youdotoo.data.local.entities.DoTooItemEntity
+import com.baljeet.youdotoo.data.local.entities.ProjectEntity
+import com.baljeet.youdotoo.data.mappers.toDoTooItem
+import com.baljeet.youdotoo.data.mappers.toProject
 import com.baljeet.youdotoo.domain.models.DoTooItem
 import com.baljeet.youdotoo.domain.use_cases.doTooItems.DeleteDoTooUseCase
 import com.baljeet.youdotoo.domain.use_cases.doTooItems.GetAllTasksUseCase
@@ -34,16 +39,49 @@ class TasksScheduleViewModel @Inject constructor(
         CoroutineScope(Dispatchers.IO).launch {
             val taskEntity = getDoTooByIdUseCase(task.id)
             val project = getProjectByIdUseCase(projectId = taskEntity.projectId)
-            if(SharedPref.isUserAPro){
-                projectsReference
-                    .document(project.id)
-                    .collection("todos")
-                    .document(task.id)
-                    .delete()
-            }
-            deleteDoTooUseCase(task, projectId = project.id)
+            deleteTask(taskEntity, project)
         }
-
     }
+
+
+    private fun deleteTask(task : DoTooItemEntity, project: ProjectEntity){
+        when(getRole(project.toProject())){
+            Roles.ProAdmin -> {
+                deleteTaskOnServerAndLocally(task)
+            }
+            Roles.Admin -> {
+                deleteTaskOnServerAndLocally(task)
+            }
+            Roles.Editor -> {
+                deleteTaskOnServerAndLocally(task)
+            }
+            Roles.Viewer -> {
+                //Do nothing can't update anything
+                //UI handles this by itself
+            }
+            Roles.Blocked -> {
+                //Do nothing can't update anything
+                //UI handles this by itself
+            }
+        }
+    }
+    private fun deleteTaskOnServerAndLocally(task: DoTooItemEntity){
+        projectsReference
+            .document(task.projectId)
+            .collection("todos")
+            .document(task.id)
+            .delete()
+            .addOnCompleteListener {
+                deleteTaskLocally(task)
+            }
+    }
+
+    private fun deleteTaskLocally(task: DoTooItemEntity){
+        CoroutineScope(Dispatchers.IO).launch {
+            deleteDoTooUseCase(task.toDoTooItem(), projectId = task.projectId)
+        }
+    }
+
+
 
 }
