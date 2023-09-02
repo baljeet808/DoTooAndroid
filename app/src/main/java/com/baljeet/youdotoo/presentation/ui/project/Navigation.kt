e9c7376661a4661b416bac5ec2386094517a9e54
Baljeet singh <Baljeet808singh@gmail.com>
2023-09-02 12:39:07 -0300

    delete project issue fixed

--- app/src/main/java/com/baljeet/youdotoo/presentation/ui/project/Navigation.kt
+++ app/src/main/java/com/baljeet/youdotoo/presentation/ui/project/Navigation.kt
@@ -10,7 +10,6 @@ import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import com.baljeet.youdotoo.common.SharedPref
 import com.baljeet.youdotoo.common.getUserIds
-import com.baljeet.youdotoo.data.local.entities.ProjectEntity
 import com.baljeet.youdotoo.data.mappers.toProject
 
 
@@ -33,60 +32,63 @@ fun NavGraphBuilder.addProjectViewDestination(
         val projectId = backStackEntry.arguments?.getString("projectId")
 
         val viewModel : ProjectViewModel = hiltViewModel()
-        val project by viewModel.getProjectById().collectAsState(initial = ProjectEntity())
+        val project by viewModel.getProjectById().collectAsState(initial = null)
         val tasks by viewModel.getProjectTasks().collectAsState(initial = listOf())
-        val users = project.let {
+        val users = project?.let {
             viewModel.getUserProfiles(it.toProject().getUserIds()).collectAsState(initial = listOf())
-        }.value
+        }?.value?:listOf()
 
-        ProjectView(
-            project = project,
-            onToggle={ task ->
-                val updatedTask = task.task.copy()
-                updatedTask.done = task.task.done.not()
-                updatedTask.updatedBy = SharedPref.userName.plus(" marked this task ").plus(if(updatedTask.done)"completed." else "not completed.")
-                viewModel.upsertTask(updatedTask, task.projectEntity.toProject())
-            },
-            tasks = tasks,
-            navigateToCreateTask = {
-                navController.navigate(
-                    "create_task/".plus(projectId)
-                )
-            },
-            users = users,
-            deleteTask = { task ->
-                viewModel.deleteTask(task.task,project.toProject())
-            },
-            deleteProject = {
-                project.toProject().let {
-                    viewModel.deleteProject(it)
-                    navController.popBackStack()
+        project?.let { projectEntity ->
+            ProjectView(
+                project = projectEntity,
+                onToggle = { task ->
+                    val updatedTask = task.task.copy()
+                    updatedTask.done = task.task.done.not()
+                    updatedTask.updatedBy = SharedPref.userName.plus(" marked this task ")
+                        .plus(if (updatedTask.done) "completed." else "not completed.")
+                    viewModel.upsertTask(updatedTask, task.projectEntity.toProject())
+                },
+                tasks = tasks,
+                navigateToCreateTask = {
+                    navController.navigate(
+                        "create_task/".plus(projectId)
+                    )
+                },
+                users = users,
+                deleteTask = { task ->
+                    viewModel.deleteTask(task.task, projectEntity.toProject())
+                },
+                deleteProject = {
+                    projectEntity.toProject().let {
+                        navController.popBackStack()
+                        viewModel.deleteProject(it)
+                    }
+                },
+                upsertProject = { updatedProject ->
+                    viewModel.updateProject(updatedProject)
+                },
+                onClickInvite = {
+                    navController.navigate("invitations/".plus(projectId))
+                },
+                navigateToEditTask = {
+                    navController.navigate("editTask/".plus(it.task.id))
+                },
+                navigateToChat = {
+                    navController.navigate(
+                        "messages/${projectId}"
+                    )
+                },
+                updateTaskTitle = { task, title ->
+                    viewModel.upsertTask(
+                        task = task.task.copy(
+                            title = title,
+                            updatedBy = SharedPref.userName.plus(" has updated this task.")
+                        ),
+                        project = projectEntity.toProject()
+                    )
                 }
-            },
-            upsertProject = { updatedProject ->
-                viewModel.updateProject(updatedProject)
-            },
-            onClickInvite = {
-                navController.navigate("invitations/".plus(projectId))
-            },
-            navigateToEditTask = {
-                navController.navigate("editTask/".plus(it.task.id))
-            },
-            navigateToChat = {
-                navController.navigate(
-                    "messages/${projectId}"
-                )
-            },
-            updateTaskTitle = { task, title->
-                viewModel.upsertTask(
-                    task = task.task.copy(
-                        title = title,
-                        updatedBy = SharedPref.userName.plus(" has updated this task.")
-                    ),
-                    project = project.toProject()
-                )
-            }
-        )
+            )
+        }
     }
 
 
